a4609233d73e8e41530bcdf717fc3fd19e1dff02
Sanjay Menaria <sanjaymenaria03@gmail.com>
2024-03-29 20:22:49 +0530

    make minor changes in menu.js

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -16,7 +16,7 @@ return (
 <Card.Header className="d-flex justify-content-between align-items-center">
 <div className="align-items-center" style={{ marginRight: 8 }}>
 <Image src={'https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Nandos_logo.svg/1200px-Nandos_logo.svg.png'} style={{ width: 150 }} />
-<p style={{ marginTop: 8, fontSize: 12, color: '#A1A1A1' }}>LG-47, 76 & 76A, Berjaya Times Square, 1, Jalan Imbi, Imbi, 55100 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur</p>
+<p style={{ marginTop: 8, fontSize: 12, color: '#A1A1A1' }}>udaipur,rajasthan(313002)</p>
 </div>
 <Button style={{ backgroundColor: '#BD2B2B', borderWidth: 0, }}>Place Order</Button>
 </Card.Header>
@@ -46,7 +46,7 @@ return (
 </Table>
 </Card.Body>
 <Card.Footer className="d-flex justify-content-between align-items-center">
-<p style={{ marginTop: 8, fontSize: 12, color: '#A1A1A1' }}>© 2022 Nandos</p>
+{/* <p style={{ marginTop: 8, fontSize: 12, color: '#A1A1A1' }}>© 2022 Nandos</p> */}
 <p style={{ marginTop: 8, fontSize: 12, color: '#A1A1A1' }}><a href="/Login">Admin Login</a> • <a href="#">Privacy Policy</a> • <a href="#">Directions</a> • <a href="#">Contact Us</a></p>
 </Card.Footer>
 </Card>
